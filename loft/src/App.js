3967527e067339e36afe14af556717b26cffe721
Azer3012 <azer0297206@gmail.com>
2021-09-03 18:24:10 +0400

    add contact page

--- loft/src/App.js
+++ loft/src/App.js
@@ -2,21 +2,40 @@ import Categories from "./pages/Categories";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeaderSection from "./pages/Header-section";
+import Carousel from "./components/Carousel";
+import AllProducts from "./pages/AllProducts";
 import Contact from "./pages/Contact";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
+      <Header/>
+      
       <div className="homepage">
-        <HeaderSection />
-        <Categories />
+        <HeaderSection/>
+        <Categories/>
+        
+
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Carousel/>
+              <AllProducts/>
+            </Route>
+            <Route exact path="/contact" component={Contact}/>
+          </Switch>
+        </Router>
+        
+        
+        
+       
       </div>
 
-      <Contact />
-
-      <Footer />
+      <Footer/>
+       
     </div>
   );
 }
